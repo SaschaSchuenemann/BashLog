d96c2c6f5587ff49d685d748c8cfc563cf9733cc
Sascha Schuenemann <sascha.schuenemann@googlemail.com>
2014-08-26 20:43:38 +0200

    cleanup and frontend improvements
    added button for select/deselect all

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -1,20 +1,41 @@
 $(function () {
     "use strict";
+
+var socket = null;
  
- // Problem with this code was, that it was not called on dynamically added elements
- /*   
-// toggle command element
-$('.command').click( function() {
-    $(this).toggleClass("selected");
-  } );
-*/
-
-
+// select item when 
 $(document).on('click', '.command' , function() {
      $(this).toggleClass("selected");
 });
 
+// (de-)select all command elements
+$('#toggleAllBtn').click( function() {
+    
+    // select all
+    if ($("#toggleAllBtn").text() == "Select All") {
+            $('.command').each(function( ) {
+                // but only those that are not yet selected
+                if(!$(this).hasClass("selected")){
+                    $(this).addClass("selected");
+                }
+            });
+            // set button to deselect
+            $("#toggleAllBtn").text("Deselect All");
+        } // deselect all
+        else {
+            // but only those that are selected
+            $('.command').each(function( ) {
+                if($(this).hasClass("selected")){
+                    $(this).removeClass("selected");
+                }
+            });
+            // set button to select
+            $("#toggleAllBtn").text("Select All");
+        }
+});
 
+// function that creates the html element for command
+// we use this function instead of css so we can extract, collect and download the elements from the dom including the style def
 var createHtmlElem = function(command){
     console.log('Create Html called with: ' + command)
     var el = '<div class="command" style=\'';
@@ -36,44 +57,38 @@ var createHtmlElem = function(command){
     el+=command;
     el+="</div>";
 
-
-
     console.log('finished element creation');
 
     return el;
 }
 
-var socket = null;
-
+// add a command to the frontend
+// is called when receiving new command via websockets
 var add = function(data) {
   console.log('Received: '+ data);
   console.log('Cmnd: ' + data.cmd);
 
   var id = data.id || '0000';
 
-  /*
-  var cmd = $('<div class="command" id="' + id + '""></div>')
-    .append(data.cmd) ;
-    */
-
-    console.log('About to call createHtmlElem');
+  // use helper function to create html element
   var cmd = createHtmlElem(data.cmd);  
   console.log("elem created: " + cmd);
 
+  // add file to dom
   $('#content')
     .append(cmd)
 }
 
+// open connection to backend using websockets
 $( document ).ready(function() {
 
       this.socket = io.connect('http://localhost/');
-
-      
 
       //Process any incoming messages
       this.socket.on('new', add);
   });
 
+// helper function that creates and then downloads a textfile from a given string
 function download(filename, text) {
     var pom = document.createElement('a');
     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -92,14 +107,19 @@ $('#exportBtn').click( function() {
     // create empty container for html code file
     var codeFile ="";
     
+    // step through all elements from class selected
     $('.selected').each(function( index ) {
 
-    // command
+    // grab command test
     var _command = $(this).text()
+
+    // create parent element for later html extraction
     var _el = $('<div></div>');
+
+    // clone command element so it can be added to parent element without destroying original dom
     var _cmdElem = $(this).clone();
 
-    // disable element
+    // deselect command element
     $(this).toggleClass("selected");
 
     // log element
@@ -109,15 +129,14 @@ $('#exportBtn').click( function() {
     logFile+=_command+"\n";
 
     // create html element for command and add to html code file
-
-
     var elemString = $(this).clone().wrap('<div/>').parent().html(); 
 
+    // add html code to code file
     codeFile+="\n"+elemString;
 
-    // create picture of this dom element / html element
+    //TODO: create picture of this dom element / html element
 
-    // send picture files to backend
+    //TODO: send picture files to backend
     });
 
 
@@ -127,6 +146,7 @@ $('#exportBtn').click( function() {
     // write html code file
     console.log("HTML Code: \n" + codeFile);
 
+    // download files
     download("log.txt",logFile);
 
     download("code.html", codeFile);
